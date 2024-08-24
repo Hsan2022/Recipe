5140e7dc1e6898a71fc58a7810271e2fd83bac0f
HSOrduno <103794936+Hsan2022@users.noreply.github.com>
2024-08-24 16:07:45 -0600

    set up navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+    return (
+        <div className="navbar container">
+            <a href="#!" className="logo">F<span>oo</span>diesHub</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!">Recipes</a>
+                <a href="#!">Settings</a>
+            </div>
+            <div className=""></div>
+
+        </div>
+    )
+}
