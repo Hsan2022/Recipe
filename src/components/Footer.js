3c9a0eab5c755b825359d55b722ee4615519cb68
HSOrduno <103794936+Hsan2022@users.noreply.github.com>
2024-09-08 21:17:25 -0600

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return(
+        <div className="footer"> 
+        <div className="footer-section">
+            <p className="title"></p>
+        </div>
+        </div>
+    )
+}
